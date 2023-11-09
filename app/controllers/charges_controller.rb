677f68708b7a6d57cca3af4ac7a81a36ee344818
Hrvoje Grdic <hrvoje.grdic1@gmail.com>
2023-11-09 10:51:24 +0100

    update config

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,7 +10,7 @@ class ChargesController < ApplicationController
 
   def create
     uri = URI('https://javelin.runpayments.io/api/v1/charge')
-    headers = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'tkn_ppc_h5cyUcYkBZFL584dkvR5QJcnNEZdtW' } # this is the secret key, hardcoded for demo purposes
+    headers = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' +  ENV.fetch('API_KEY') }
     response = Net::HTTP.post(uri, params.merge(mid: '496160873888').to_json, headers)
 
     if response.code.to_s == '200'
